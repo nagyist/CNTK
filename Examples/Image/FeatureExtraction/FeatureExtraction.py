df5fa6cc18ae7f837fd93746c9d7c44602141d9d
Philipp Kranen <pkranen@microsoft.com>
2017-05-17 17:56:51 +0200

    fixed mb source in featurizer example

--- Examples/Image/FeatureExtraction/FeatureExtraction.py
+++ Examples/Image/FeatureExtraction/FeatureExtraction.py
@@ -7,6 +7,7 @@
 from __future__ import print_function
 import os
 import numpy as np
+import cntk as C
 from cntk import load_model, combine
 from cntk.logging import graph
 from cntk.logging.graph import get_node_outputs
@@ -14,10 +15,11 @@ import cntk.io.transforms as xforms
 
 def create_mb_source(image_height, image_width, num_channels, map_file):
     transforms = [xforms.scale(width=image_width, height=image_height, channels=num_channels, interpolations='linear')]
-    return MinibatchSource(ImageDeserializer(map_file,
-        StreamDefs(features=StreamDef(field='image', transforms=transforms))), # first column  in map file is referred to as 'image'
-        randomize=False)                                                       # second column is labels and is ignored
-
+    return C.io.MinibatchSource(C.io.ImageDeserializer(map_file,
+        C.io.StreamDefs(
+            features=C.io.StreamDef(field='image', transforms=transforms), # first column  in map file is referred to as 'image'
+            labels=C.io.StreamDef(field='label', shape=1000))),            # second column is labels and is ignored
+        randomize=False)
 
 def eval_and_write(model_file, node_name, output_file, minibatch_source, num_objects):
     # load model and pick desired node as output
@@ -34,13 +36,13 @@ def eval_and_write(model_file, node_name, output_file, minibatch_source, num_obj
             output = output_nodes.eval(mb[features_si])
 
             # write results to file
-            out_values = output[0,0].flatten()
+            out_values = output[0].flatten()
             np.savetxt(results_file, out_values[np.newaxis], fmt="%.6f")
 
 if __name__ == '__main__':
     # define location of model and data and check existence
     base_folder = os.path.dirname(os.path.abspath(__file__))
-    model_file  = os.path.join(base_folder, "..", "PretrainedModels", "ResNet_18.model")
+    model_file  = os.path.join(base_folder, "..", "PretrainedModels", "ResNet18_ImageNet_CNTK.model")
     map_file    = os.path.join(base_folder, "..", "DataSets", "Grocery", "test.txt")
     os.chdir(os.path.join(base_folder, "..", "DataSets", "Grocery"))
     if not (os.path.exists(model_file) and os.path.exists(map_file)):
@@ -56,6 +58,8 @@ if __name__ == '__main__':
     # use this to print all node names of the model (and knowledge of the model to pick the correct one)
     # node_outputs = get_node_outputs(load_model(model_file))
     # for out in node_outputs: print("{0} {1}".format(out.name, out.shape))
+    from cntk.logging.graph import plot
+    plot(load_model(model_file), "graph.png")
 
     # use this to get 1000 class predictions (not yet softmaxed!)
     # node_name = "z"
